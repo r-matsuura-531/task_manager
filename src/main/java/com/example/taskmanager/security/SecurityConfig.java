b0d7ffe3db45ac04a98bf934cb798a5c1e438092
matsuura ryoma <r-matsuura@zyyx.jp>
2025-12-22 17:24:17 +0900

    認証STEP1_簡易的なログイン機能を作成

--- src/main/java/com/example/taskmanager/security/SecurityConfig.java
+++ src/main/java/com/example/taskmanager/security/SecurityConfig.java
@@ -1,19 +1,50 @@
 package com.example.taskmanager.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-				.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-				.csrf(csrf -> csrf.disable());
-		return http.build();
-	}
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .formLogin(formLogin -> formLogin
+            .loginPage("/login")
+            .loginProcessingUrl("/login")
+            .defaultSuccessUrl("/")
+            .permitAll()
+        )
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login")
+        )
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/login",
+                "/css/**",
+                "/js/**")
+            .permitAll()
+            .anyRequest().authenticated()
+        );
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails user = User.withUsername("user")
+        .password("{noop}password") // パスワード平文（初級）
+        .roles("USER")
+        .build();
+    return new InMemoryUserDetailsManager(user);
+  }
 }
